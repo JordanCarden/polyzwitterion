e7a0fb2434c2c2efa62088b43bcb339410f5a3db
JordanCarden <jcarde7@lsu.edu>
2025-08-06 09:44:28 -0500

    added results and ignored simulations

--- 2_scripts/submit.sh
+++ 2_scripts/submit.sh
@@ -6,12 +6,12 @@
 #SBATCH --ntasks-per-node=4
 #SBATCH --cpus-per-task=16
 #SBATCH -t 72:00:00
-#SBATCH -o /work/jcarde7/polyzwitterion/3_simulations/generation_01/out_%j.txt
-#SBATCH -e /work/jcarde7/polyzwitterion/3_simulations/generation_01/err_%j.txt
+#SBATCH -o /work/jcarde7/polyzwitterion/3_simulations/generation_30/out_%j.txt
+#SBATCH -e /work/jcarde7/polyzwitterion/3_simulations/generation_30/err_%j.txt
 #SBATCH --distribution=block:block
 
 NAMD=/work/jcarde7/polyzwitterion/NAMD_2.14_Linux-x86_64-multicore/namd2
-BASE=/work/jcarde7/polyzwitterion/3_simulations/generation_01
+BASE=/work/jcarde7/polyzwitterion/3_simulations/generation_30
 SIMS=(child_01 child_02 child_03 child_04 child_05 child_06 child_07 child_08 child_09 child_10 child_11 child_12)
 
 for s in "${SIMS[@]}"; do
